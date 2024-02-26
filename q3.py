6119a2254659a45a00c03033d520c3147739f08a
Farzanmrz <farzanmrz@gmail.com>
2024-02-26 17:45:11 -0500

    Cleaned up q3 file

--- q3.py
+++ q3.py
@@ -1,13 +1,7 @@
 # Imports
 import ConvolutionalLayer, MaxPoolLayer, FlatteningLayer, FullyConnectedLayer, LogisticSigmoidLayer, LogLoss
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pyplot as plt
-
-
-
-
 
 # Create synthetic data images for vertical and horizontal images
 vimg = np.zeros((40, 40))
@@ -16,110 +10,159 @@ vimg[ :, 4 ] = 1
 himg[ 32, : ] = 1
 
 # Create the input tensor
-img =np.array([vimg, himg])
+img = np.array([ vimg, himg ])
+
+# Create target vector y
+y = np.array([ [ 0 ], [ 1 ] ])
 
 # Define layers
-layers = [ConvolutionalLayer.ConvolutionalLayer(9, 9), MaxPoolLayer.MaxPoolLayer(4, 4),
-          FlatteningLayer.FlatteningLayer(), FullyConnectedLayer.FullyConnectedLayer(64, 1),
-          LogisticSigmoidLayer.LogisticSigmoidLayer(), LogLoss.LogLoss()]
+layers = [ ConvolutionalLayer.ConvolutionalLayer(9, 9),
+           MaxPoolLayer.MaxPoolLayer(4, 4),
+           FlatteningLayer.FlatteningLayer(),
+           FullyConnectedLayer.FullyConnectedLayer(64, 1),
+           LogisticSigmoidLayer.LogisticSigmoidLayer(),
+           LogLoss.LogLoss() ]
 
 # Store initial kernel
 init_kernel = layers[ 0 ].getWeights()
 
-# Create target vector y
-y = np.array([[0],[1]])
 
-def fprop(layers, x, y):
+def fprop( layers, x, y ):
+	"""
+	Performs forward propagation through the network, excluding the last layer,
+	and calculates the loss using the last layer.
 
-    # Initialize activation with input x
-    activation = x
+	This function propagates an input through all layers of the network except
+	the last one, then uses the last layer to compute the loss based on the
+	output of the preceding layer and the true labels.
 
-    # Propagate through all layers except the last
-    for layer in layers[ :-1 ]:
-        activation = layer.forward(activation)
+	:param layers: List of layers in the CNN, configured for forward propagation.
+	:param x: Input data to the network, numpy array of shape (n_samples, height, width).
+	:param y: True labels for the input data, numpy array of shape (n_samples, 1).
 
-    # Calculate loss using the last layer
-    loss = layers[ -1 ].eval(y, activation)
+	:return: A tuple containing the output from the last but one layer and the loss
+			 calculated using the last layer.
+	"""
 
-    # Return the result of last layer and loss
-    return activation, loss
+	# Initialize activation with input x
+	activation = x
 
-# Function to run the CNN
-def runCNN(layers, img, y):
+	# Propagate through all layers except the last
+	for layer in layers[ :-1 ]:
+		activation = layer.forward(activation)
 
-    fin_l = []
+	# Calculate loss using the last layer
+	loss = layers[ -1 ].eval(y, activation)
 
-    prev = 0
-
-    for epoch in range(2000):
-
-        # Forward propogation
-        yhat, loss = fprop(layers, img, y)
-        fin_l.append(loss)
-
-        ############ Backward pass through the convolutional layer ############
-
-        # Log loss layer
-        ll_grad = layers[ 5 ].gradient(y, yhat)
-
-        # Logistic sigmoid layer
-        ls_grad = layers[ 4 ].backward(ll_grad)
+	# Return the result of last layer and loss
+	return activation, loss
 
 
-        # Fully connected layer
-        fc_grad = layers[ 3 ].backward(ls_grad)
+def bProp( layers, Y, h, t = 0 ):
+	"""
+	Performs backward propagation through the network for gradient calculation and weight updates.
+
+	This function computes the gradient of the loss with respect to the weights of each layer in the network
+	by propagating the error backwards. It updates the weights of the layers based on the computed gradients.
+	The process begins from the last layer and proceeds through each layer up to the first, updating weights
+	as necessary.
+
+	:param layers: A list of layers in the network, including convolutional, fully connected, and loss layers.
+	:param Y: The true labels for the input data, numpy array of shape (n_samples, 1).
+	:param h: The output from the last layer during forward propagation, numpy array of shape (n_samples, n_classes).
+	:param t: The current epoch or iteration number, used for updating weights with time-dependent learning rates.
+
+	Note: This function updates the weights of the network in-place and does not return any value.
+	"""
+
+	# Calculate initial gradient based on the loss
+	grad = layers[ -1 ].gradient(Y, h)
+
+	# Iterate backwards through layers, skipping the first layer
+	for i in range(len(layers) - 2, 0, -1):
+
+		# Calculate new gradient for the current layer
+		newgrad = layers[ i ].backward(grad)
+
+		# Update weights if the layer is a FullyConnectedLayer
+		if isinstance(layers[ i ], FullyConnectedLayer.FullyConnectedLayer):
+			layers[ i ].updateWeights(grad, t)
+
+		# Update gradient
+		grad = newgrad
+
+	# Update gradient for final Convolutional layer
+	layers[ 0 ].updateWeights(grad, t)
 
 
-        layers[3].updateWeights(ls_grad,epoch)
+def runCNN( layers, img, y ):
+	"""
+	Trains a Convolutional Neural Network on a given dataset and returns the loss history and learned kernel weights.
 
-        # Flattening layer
-        fl_grad = layers[ 2 ].backward(fc_grad)
+	The function performs forward propagation to calculate the loss, then backward propagation to update the weights
+	of the layers. It prints the loss at specified intervals and checks for convergence by comparing the change in loss
+	across epochs. Training stops when the model converges or when the maximum number of epochs is reached.
 
-        # Max pooling layer
-        grad = layers[ 1 ].backward(fl_grad)
+	:param layers: A list of layers that constitute the CNN. Each layer should support forward and backward operations.
+	:type layers: List[Layer]
+	:param img: The input data (images) on which the CNN will be trained. It should be a numpy array of shape (n, height, width),
+				where n is the number of images.
+	:type img: numpy.ndarray
+	:param y: The target outputs (labels) for the input data. It should be a numpy array of shape (n, 1), where n is the number of images.
+	:type y: numpy.ndarray
 
-        # Convolutional layer update weights
-        layers[ 0 ].updateWeights(grad, epoch)
+	:return: A tuple containing two elements:
+			 - The first element is a list of loss values recorded at each epoch during training.
+			 - The second element is the numpy array representing the weights of the first convolutional layer after training.
+	:rtype: (list, numpy.ndarray)
+	"""
+	fin_l = [ ]
+	prev = 0
 
-        if epoch % 100 == 0:
-            print("Epoch: ", epoch + 1)
-            print("Loss: ", loss)
+	# Loop through epochs
+	for epoch in range(5000):
 
-        if epoch > 100 and np.abs(loss - prev) < 1e-10:
-            print("Converged at epoch: ", epoch + 1)
-            print("Loss: ", loss)
-            break
+		# Forward propagation
+		yhat, loss = fprop(layers, img, y)
+		fin_l.append(loss)
 
-        prev = loss
+		# Backward propagation
+		bProp(layers, y, yhat, epoch)
+
+		# Print loss every 500 epochs
+		if epoch % 500 == 0:
+			print("Epoch: ", epoch + 1)
+			print("Loss: ", loss)
+
+		# Terminating condition
+		if np.abs(loss - prev) < 1e-8:
+			print("Converged at epoch: ", epoch + 1)
+			print("Loss: ", loss)
+			break
+
+		prev = loss
+
+	return fin_l, layers[ 0 ].getWeights()
 
 
-    return fin_l, layers[ 0 ].getWeights()
+# Train the CNN
+loss, fin_kern = runCNN(layers, img, y)
 
-
-
-
-loss, res = runCNN(layers, img, y)
-
-
-
-fig, ax = plt.subplots(2, 2, figsize = (10, 5))
-ax[1][0].imshow(init_kernel, cmap = 'gray')
-ax[1][1].imshow(res, cmap = 'gray')
-ax[ 0 ][0].imshow(vimg, cmap = 'gray')
-ax[ 0 ][1].imshow(himg, cmap = 'gray')
-ax[ 0 ][0].axis('off')
-ax[ 1 ][0].axis('off')
-ax[ 0 ][1].axis('off')
-ax[ 1 ][1].axis('off')
+# Plot initial and final kernels
+fig, ax = plt.subplots(1, 2, figsize = (10, 5))
+ax[ 0 ].imshow(init_kernel, cmap = 'gray')
+ax[ 1 ].imshow(fin_kern, cmap = 'gray')
+ax[ 0 ].axis('off')
+ax[ 1 ].axis('off')
+plt.savefig("kernels.png")
 plt.show()
 
-epochs = list(range(1, len(loss) + 1))
-
-# Plotting the log loss against epochs
-plt.figure(figsize=(10, 5))
-plt.plot(epochs, loss, linestyle='-', color='b')
+# Plot the log loss against epochs
+plt.figure(figsize = (10, 5))
+plt.plot(list(range(1, len(loss) + 1)), loss, linestyle = '-', color = 'b')
 plt.title('Log Loss per Epoch')
 plt.xlabel('Epoch')
 plt.ylabel('Log Loss')
 plt.grid(True)
+plt.savefig("LogLoss.png")
 plt.show()
